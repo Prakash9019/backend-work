7252e7e4717531049df78c1be37097355c57d1fc
Irfanabul1108 <indianirfan1108@gmail.com>
2025-03-20 09:26:42 +0530

    backend-work

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// vite.config.js
+
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    outDir: 'build'  // Change this from "dist" to "build"
+  }
+});
