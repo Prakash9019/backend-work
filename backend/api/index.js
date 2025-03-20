5cc67151172099fe353653e7026f870e90f8332d
Prakash9019 <plsprakash2003@gmail.com>
2025-03-20 15:39:25 +0530

    added

--- backend/api/index.js
+++ backend/api/index.js
@@ -56,8 +56,8 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
 
 module.exports = app;
